73fd27c0872f2435f9fbb159e0ad9361fd9693cc
Aqsa Gull <aqsabravegirl3@gmail.com>
2024-03-19 22:43:03 +0500

    first commit

---
+++ func.ts
@@ -0,0 +1,131 @@
+// Function with only console 
+
+// function nuddles(){
+// let cooked:string ="Red chilli" + ", " +"Salt" + ", "+ "Water" + ", " +"Souces" +  ", "+ "Maggi Masala" 
+
+// console.log(cooked);
+// }
+
+// nuddles()
+
+
+
+
+
+// Function with returinig fountion values  
+
+
+// function nuddlesEat():string{
+//     let cooked:string = "Red chilli" +", " + "Salt" +", " + "Water" + ", " + "Souces" +", " + "Maggi Masala"
+    
+//     return cooked
+    
+//     }
+    
+//    let eat:string=nuddlesEat() 
+//    console.log(eat)
+
+
+
+
+
+
+// Funtion with parameters & Argumenet
+
+
+// function nuddlesEatRecipe(ingredient1: string, ingredient2: string, ingredient3: string, ingredient4: string, ingredient5: string, ingredient6: string): string {
+//     let cooked: string = ingredient1 + ", " + ingredient2 + ", " + ingredient3 + ", " + ingredient4 + ", " + ingredient5 + ", " + ingredient6;
+//     return cooked;
+// }
+
+// const eatable: string = nuddlesEatRecipe("Red chilli", "Salt", "Water", "Souces", "Maggi Masala", "Lemon"); // Ingredient's of noodles
+// console.log(eatable);
+
+
+
+
+
+// Function with if else statement 
+
+// function fruitChat(num1:number,num2:number,num3:number){
+
+//     if (num1 && num2 && num3 ){
+// console.log("you can eat")
+//     }else{
+//         console.log("you cant eat")
+//     }
+//     return num1 + num2 + num3 
+// }
+
+// const fruitChatEat=fruitChat(3,4,6)  // output you can eat
+
+
+
+
+
+// function fruitChat1(num1:number,num2:number,num3:number){
+
+//     if (num1 && num2 && num3 ){
+// console.log("you can eat")
+//     }else{
+//         console.log("you cant eat")
+//     }
+//     return num1 + num2 + num3 
+// }
+
+// let fruitChatEat1=fruitChat1(0,4,6)  // output you cant eat
+
+
+
+
+
+// fountion with if else if else statement
+
+// function checkNumberType(num: number): string {
+//     if (num > 10) {
+//         return "Positive";
+//     } else if (num < 0) {
+//         return "Negative";
+//     } else {
+//         return "Zero";
+//     }
+// }
+
+// // Example usage:
+// console.log(checkNumberType(20)); // Output: Positive
+// console.log(checkNumberType(-3)); // Output: Negative
+// console.log(checkNumberType(0)); // Output: Zero
+
+
+
+
+
+
+// function fruitChat2(num1:number,num2:number,num3:number){
+
+//     if (num1 && num2 || num3 ){
+// console.log("you can eat")
+//     }else{
+//         console.log("you cant eat")
+//     }
+//     return num1 + num2 + num3 
+// }
+
+// const fruitChatEat2=fruitChat2(0,0,6)  // output you can eat
+
+
+
+
+
+
+// function fruitChat3(num1:number,num2:number,num3:number){
+
+//     if (num1 != num2 && num3 ){
+// console.log("you can eat")
+//     }else{
+//         console.log("you cant eat")
+//     }
+//     return num1 + num2 + num3 
+// }
+
+// let fruitChatEat3=fruitChat3(9,0,0)  // output you cant eat
